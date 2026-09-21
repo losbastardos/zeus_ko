97e969938393c88add3bb5f68cb0e881e17f7553
losbastardos <marek.suchy@gmail.com>
2026-09-21 15:02:56 +0200

    tb: handle uniform non-constant symbol trees in WDL probe

--- src/tb.asm
+++ src/tb.asm
@@ -59,6 +59,8 @@ tb_wdl_pairs_idxbits: resb 1      ; debug: idxbits (WDL)
 tb_dtz_pairs_idxbits: resb 1      ; debug: idxbits (DTZ)
 tb_wdl_pairs_is_const: resb 1     ; debug: 1 ak constant table (WDL)
 tb_dtz_pairs_is_const: resb 1     ; debug: 1 ak constant table (DTZ)
+tb_sym_cache_state: resb 4096     ; 0=unknown,1=visiting,2=ok,3=mixed/invalid
+tb_sym_cache_value: resb 4096     ; cache leaf hodnot pre symboly
 
 section .text
 
@@ -475,6 +477,174 @@ tb_parse_pairs_minlen:
     ret
 
 ; ============================================================
+; tb_sym_leaf_uniform - zisti, ci symbol v sympat strome dekoduje
+; na unikatnu terminalnu leaf hodnotu.
+; Vstup: rdi = sympat ptr, esi = num_syms, edx = sym_idx
+; Vystup: eax = 1 uspech / 0 fail|mixed
+;         dl  = leaf value pri uspechu
+; Pozn.: pouziva tb_sym_cache_state/value.
+; ============================================================
+tb_sym_leaf_uniform:
+    push rbx
+    push rcx
+    push r8
+    push r9
+    push r10
+
+    mov ebx, edx
+
+    cmp ebx, esi
+    jae .fail
+
+    lea r8, [tb_sym_cache_state]
+    movzx ecx, byte [r8 + rbx]
+    cmp ecx, 2
+    je .cached_ok
+    cmp ecx, 3
+    je .fail
+    cmp ecx, 1
+    je .fail
+
+    mov byte [r8 + rbx], 1
+
+    lea r9, [rbx + rbx*2]
+    lea r9, [rdi + r9]
+
+    movzx ecx, byte [r9 + 1]
+    movzx eax, byte [r9 + 2]
+    shl eax, 4
+    mov r10d, eax
+    mov eax, ecx
+    shr eax, 4
+    or r10d, eax                 ; s2
+
+    cmp r10d, 0x0fff
+    jne .non_terminal
+
+    movzx edx, byte [r9]
+    lea rcx, [tb_sym_cache_value]
+    mov [rcx + rbx], dl
+    mov byte [r8 + rbx], 2
+    mov eax, 1
+    jmp .done
+
+.non_terminal:
+    mov eax, ecx
+    and eax, 0x0f
+    shl eax, 8
+    movzx ecx, byte [r9]
+    or eax, ecx                  ; s1
+    mov r11d, eax
+
+    mov edx, r11d
+    call tb_sym_leaf_uniform
+    test eax, eax
+    jz .mark_fail
+    mov ecx, edx                 ; val1
+
+    mov edx, r10d
+    call tb_sym_leaf_uniform
+    test eax, eax
+    jz .mark_fail
+    cmp dl, cl
+    jne .mark_fail
+
+    ; success: cache value for current symbol
+    lea r9, [tb_sym_cache_value]
+    mov [r9 + rbx], cl
+    mov byte [r8 + rbx], 2
+    movzx edx, cl
+    mov eax, 1
+    jmp .done
+
+.cached_ok:
+    lea r9, [tb_sym_cache_value]
+    movzx edx, byte [r9 + rbx]
+    mov eax, 1
+    jmp .done
+
+.mark_fail:
+    mov byte [r8 + rbx], 3
+
+.fail:
+    xor eax, eax
+
+.done:
+    pop r10
+    pop r9
+    pop r8
+    pop rcx
+    pop rbx
+    ret
+
+; ============================================================
+; tb_pairs_uniform_root - skusi odvodit globalne konstantnu leaf hodnotu
+; pre setup_pairs strom (aj pri idxbits>0), ak vsetky root symboly
+; dekoduju na rovnaku hodnotu.
+; Vstup: rdi = sympat ptr, esi = num_syms
+; Vystup: eax = 1 uspech / 0 fail
+;         dl  = leaf value pri uspechu
+; ============================================================
+tb_pairs_uniform_root:
+    push rbx
+    push rcx
+    push r8
+    push r12
+
+    test rdi, rdi
+    jz .fail
+    test esi, esi
+    jle .fail
+    cmp esi, 4096
+    ja .fail
+
+    ; reset cache state len pre aktivny rozsah num_syms
+    lea r8, [tb_sym_cache_state]
+    xor ecx, ecx
+.clr:
+    cmp ecx, esi
+    jae .scan
+    mov byte [r8 + rcx], 0
+    inc ecx
+    jmp .clr
+
+.scan:
+    xor ebx, ebx
+    mov r12d, -1
+.scan_loop:
+    cmp ebx, esi
+    jae .ok
+    mov edx, ebx
+    call tb_sym_leaf_uniform
+    test eax, eax
+    jz .fail
+    cmp r12d, -1
+    jne .cmp_prev
+    movzx r12d, dl
+    jmp .next
+.cmp_prev:
+    cmp dl, r12b
+    jne .fail
+.next:
+    inc ebx
+    jmp .scan_loop
+
+.ok:
+    mov dl, r12b
+    mov eax, 1
+    jmp .done
+
+.fail:
+    xor eax, eax
+
+.done:
+    pop r12
+    pop r8
+    pop rcx
+    pop rbx
+    ret
+
+; ============================================================
 ; tb_try_constant_wdl - skusi priamy WDL decode z setup_pairs streamu
 ; Vstup: r13d = white non-king type (0/typ), r14d = black non-king type (0/typ)
 ; Vystup: eax = TB_WIN/TB_DRAW/TB_LOSS alebo TB_NOT_FOUND
@@ -544,9 +714,12 @@ tb_try_constant_wdl:
     movzx eax, byte [rbx + 2]      ; idxbits
     test eax, eax
     jz .pc0_minlen
-    cmp r9d, 1
-    jne .not_found
-    movzx r10d, byte [r10]
+    mov rdi, r10
+    mov esi, r9d
+    call tb_pairs_uniform_root
+    test eax, eax
+    jz .not_found
+    movzx r10d, dl
     jmp .pc0_ok
 .pc0_const:
 .pc0_minlen:
@@ -567,9 +740,12 @@ tb_try_constant_wdl:
     movzx eax, byte [rbx + 2]      ; idxbits
     test eax, eax
     jz .pc1_minlen
-    cmp r9d, 1
-    jne .not_found
-    movzx r11d, byte [r10]
+    mov rdi, r10
+    mov esi, r9d
+    call tb_pairs_uniform_root
+    test eax, eax
+    jz .not_found
+    movzx r11d, dl
     jmp .pc1_ok
 .pc1_const:
 .pc1_minlen:
@@ -692,15 +868,19 @@ tb_try_constant_wdl:
     cmp ecx, r10d
     jne .pawn_next
 
-    ; target slot: constant, alebo non-constant idxbits==0
+    ; target slot: constant, non-constant idxbits==0,
+    ; alebo uniform-root strom pri idxbits>0.
     cmp eax, 1
     je .pawn_const
     movzx eax, byte [rbx + 2]      ; idxbits
     test eax, eax
     jz .pawn_minlen
-    cmp r9d, 1
-    jne .not_found
-    movzx r10d, byte [r10]
+    mov rdi, r10
+    mov esi, r9d
+    call tb_pairs_uniform_root
+    test eax, eax
+    jz .not_found
+    movzx r10d, dl
     jmp .raw_to_class
 .pawn_const:
 .pawn_minlen:
